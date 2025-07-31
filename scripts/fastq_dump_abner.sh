43bc5eaa86fc8cc637669090c8d57808ebe035db
abportillo02 <abportillo@coh.org>
2025-07-31 13:08:37 -0700

    fastq_dump

---
+++ scripts/fastq_dump_abner.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=d1_fastq
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=abportillo@coh.org
+#SBATCH -n 4
+#SBATCH -N 1
+#SBATCH -p all
+#SBATCH --mem=50G
+#SBATCH --time=05:30:00
+#SBATCH --output=dl_{sampleSRR}_%j.log
+# Path to your SRR accession list (one SRR per line)
+
+
+SRR_LIST=~/scripts/SRR_Acc_List.txt
+
+# Output directory (optional, create if needed)
+OUTDIR=./home/abportillo/github_repo/RNA_seq_Bcell/scripts/fastq_bcell
+mkdir -p "$OUTDIR"
+
+# Loop through each SRR and run fasterq-dump
+while read -r SRR; do
+    echo "Processing $SRR"
+    fasterq-dump "$SRR" -O "$OUTDIR"
+done < "$SRR_LIST"
