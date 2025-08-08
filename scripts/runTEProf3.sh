c3ab4b960f7b6621077f7884c18bb449c4dad24f
abportillo02 <abportillo@coh.org>
2025-08-08 15:24:08 -0700

    runTEProf3 updated

--- scripts/runTEProf3.sh
+++ scripts/runTEProf3.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-folder=$1
-# Define the directory:
+# Check if file with folder names is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <sampleNames.txt>"
+  exit 1
+fi
+
+folder_list=$1
+outdir="/home/abportillo/github_repo/RNA_seq_Bcell/scripts/raw_fastq_bcell/rnaPreprocess/Transcript_finder"  # single shared folder
+
+# Make sure the one output directory exists
+mkdir -p "${outdir}"
+
 while IFS= read -r folder; do
+  # Skip empty lines
+  [ -z "$folder" ] && continue
+
   echo "Creating bash script for folder: $folder"
-
-  # teprof3Dir="/net/nfs-irwrsrchnas01/labs/dschones/bioresearch/Abner/DCIS/project/Transcript_finder/TEProf3_TEdetect"
-
-  # cd ${teprof3Dir}
-
-  outdir=/net/nfs-irwrsrchnas01/labs/dschones/bioresearch/Abner/DCIS/project/Transcript_finder/${folder}
-
+  
   {
   echo -e "#!/bin/bash
 #SBATCH --job-name=${folder}_TEProf3
@@ -23,28 +30,23 @@ while IFS= read -r folder; do
 #SBATCH --time=48:00:00
 #SBATCH --output=${outdir}/${folder}_TEProf3.log
 
-cd ${outdir}
-
-# Softlink data to 4 folders 
-
-# ls /net/nfs-irwrsrchnas01/labs/dschones/bioresearch/Abner/DCIS/project/*/*_sorted_nr_sorted.bam.bai | sed -n '1,10p' | xargs -I {} ln -s "/net/nfs-irwrsrchnas01/labs/dschones/bioresearch/Abner/DCIS/project/*/*_sorted_nr_sorted.bam.bai" /net/nfs-irwrsrchnas01/labs/dschones/bioresearch/Abner/DCIS/project/Transcript_finder/TEProf3_TEdetect1
+cd ${outdir} 
 
 source /home/abportillo/.bashrc
 module load Mamba/24.3.0-0
 mamba activate /home/abportillo/.conda/envs/mamba_abner_BC
 
- 
 # get sample_manifest file
-# rm sample_manifest.txt
-# find . -maxdepth 1 -name "*.bam"  | while read file ; do xbase=$(basename $file); sample_name=${xbase/_sorted_nr_sorted.bam/} ; echo -e "${sample_name}\tshort\t${xbase}" >> sample_manifest.txt; done ;
-# find . -maxdepth 1 -name "*.tab"  | while read file ; do xbase=$(basename $file); sample_name=${xbase/_SJ.out.tab/} ; echo -e "${sample_name}\tSJ\t${xbase}" >> sample_manifest.txt; done ;
-# find . -maxdepth 1 -name "*.gtf"  | while read file ; do xbase=$(basename $file); sample_name=${xbase/.gtf/} ; echo -e "${sample_name}\tgtf\t${xbase}" >> sample_manifest.txt; done
+rm -f sample_manifest.txt
+find . -maxdepth 1 -name "*.bam"  | while read file ; do xbase=$(basename $file); sample_name=${xbase/_sorted_nr_sorted.bam/} ; echo -e "${sample_name}tshortt${xbase}" >> sample_manifest.txt; done
+find . -maxdepth 1 -name "*.tab"  | while read file ; do xbase=$(basename $file); sample_name=${xbase/_SJ.out.tab/} ; echo -e "${sample_name}tSJt${xbase}" >> sample_manifest.txt; done
+find . -maxdepth 1 -name "*.gtf"  | while read file ; do xbase=$(basename $file); sample_name=${xbase/.gtf/} ; echo -e "${sample_name}tgtft${xbase}" >> sample_manifest.txt; done
 
 # reset all folders if error log
-teprof3 -f sample_manifest.txt --reset
+# teprof3 -f sample_manifest.txt --reset
 
 
-# run teprof3:ZZ
+# run teprof3:
 # Assemble step (-as) \ # run Process assemble step (-ps) \ # run Filter transcripts (-fs) \\ 
 # run Mega assembly (--tacothread) \ # run Quantification (-qs)
 teprof3 -f sample_manifest.txt -s 10 -am 1 -as 10 --assemblethread 4 --assemblestrand 1 --assemblejunctionread 2 \
@@ -55,14 +57,6 @@ teprof3 -f sample_manifest.txt -s 10 -am 1 -as 10 --assemblethread 4 --assembles
   -qs 10 --quansamplenumbercon 100 --quanmode 1 --quanreadlength 75
 
 mamba deactivate  "
- 
-  } > ${outdir}/${folder}_runTEProf3.sh
-#  cd ${outdir}
-#  sbatch ${folder}_runTEProf3.sh
-done < ${folder}
+  } > "${outdir}/${folder}_runTEProf3.sh"
 
-# # change .bashrc
-# vim ~/.bashrc 
-# # add these lines:
-# PATH=$PATH:/home/abportillo/.conda/envs/mamba_abner_BC/bin/TEProf3/bin/
-# export PATH="/home/abportillo/.conda/envs/taco_env/bin:$PATH"
+done < "$folder_list"
