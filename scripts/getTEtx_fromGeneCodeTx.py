3532191a4b454e0c820f2acfe13ddc82cdfe918f
abportillo02 <abportillo@coh.org>
2025-08-11 16:00:22 -0700

    test

--- scripts/getTEtx_fromGeneCodeTx.py
+++ scripts/getTEtx_fromGeneCodeTx.py
@@ -45,10 +45,11 @@ for sample in samples:
                         # Write fields similar to your original script
                         # line[0] = chrom, [3]=start, [4]=end, [9] is attribute, [6]=strand
                         out_file.write(
+    out_file.write(
     f"{fields[0]}\t{fields[3]}\t{fields[4]}\t"
-    f"{fields[9].split('\"')[1]}\t{tx_id}\t{fields[6]}\t"
-    f"{bed_dict[tx_id]}\t{fields[-5].split('\"')[1]}\t"
-    f"{fields[-3].split('\"')[1]}\t{fields[-1].split('\"')[1]}\n"
+    f"{gene_id}\t{tx_id}\t{fields[6]}\t"
+    f"{bed_dict[tx_id]}\t{gene_name}\t"
+    f"{gene_type}\t{extra_info}\n"
 )
 
         print(f"Written filtered BED to: {out_path}")
