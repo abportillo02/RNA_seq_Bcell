0e2252ccd14f9465bb32d2756287910634e6fde3
abportillo02 <abportillo@coh.org>
2025-08-18 14:21:58 -0700

    HERV filter

---
+++ scripts/HERV_filter.py
@@ -0,0 +1,45 @@
+import sys
+import os
+
+# /home/abportillo/github_repo/RNA_seq_Bcell/scripts/sampleNames.txt
+sample = sys.argv[1]
+# outpath = os.path.join("/home/abportillo/github_repo/RNA_seq_Bcell/scripts/raw_fastq_bcell/rnaPreprocess"
+#                        "/counts_tx", sample)
+outPath = sys.argv[2]
+# outPath = os.path.join(f"{outPath}", sample)
+inputPath = sys.argv[3]
+
+dict = {}
+# input = open("/home/abportillo/github_repo/TEProf3/reference/journal.pcbi.1006453.s006", "r")
+input = open(f"{inputPath}", "r")
+
+for line in input:
+    line = line.strip()
+    line = line.split()
+    dict[line[3]] = line[4]
+
+input.close()
+
+# get tx counts info for TE transcripts
+# Read sample names from your file
+with open(sample, "r") as f:
+    samples = [line.strip() for line in f]
+
+# Loop over samples
+for s in samples:
+   bed_file = os.path.join(outPath, s, f"{s}_Gencode_HERV.bed")
+
+    input = open(f"/home/abportillo/github_repo/TEProf3/reference/journal.pcbi.1006453.s006", "r")
+    output = open(bed_file, "w")
+
+    for line in input:
+        line = line.strip()
+        line = line.split()
+        if line[2] == 'transcript':
+            tx_id = line[11].split('"')[1]
+            if tx_id in dict.keys():
+                output.write(line[0] + '\t' + line[3] + '\t' + line[4] + '\t' +
+                            line[9].split('"')[1] + '\t' + tx_id + '\t' +
+                            line[6] + "\t" + dict[tx_id] + "\t" +
+                            line[-5].split('"')[1] + '\t' + line[-3].split('"')[1] + '\t' +
+                            line[-1].split('"')[1] + '\n')
