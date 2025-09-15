c56f4742d8c1c22dfc9c1d8950a2fd1cf30e7698
abportillo02 <abportillo@coh.org>
2025-09-15 11:38:00 -0700

    chrom dir

--- scripts/bed_to_bigwig_step2.sh
+++ scripts/bed_to_bigwig_step2.sh
@@ -13,7 +13,7 @@
 dir="/home/abportillo/github_repo/RNA_seq_Bcell/output/chip_exo/KLF4/kzfp_peaks"
 
 # Path to chromosome sizes file
-chrom_sizes="hg38.chrom.sizes"  
+chrom_sizes="/home/abportillo/github_repo/RNA_seq_Bcell/output/chip_exo/KLF4/kzfp_peaks/hg38.chrom.sizes"  
 
 # Loop through each sorted BED file
 for file in "${dir}"/*.sorted.bed; do
