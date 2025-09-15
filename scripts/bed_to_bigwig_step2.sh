72596359952b6b80efff3d354b515e17bee41a11
abportillo02 <abportillo@coh.org>
2025-09-15 11:36:12 -0700

    bash addition

--- scripts/bed_to_bigwig_step2.sh
+++ scripts/bed_to_bigwig_step2.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=bed_to_bigwig_2
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=abportillo@coh.org
+#SBATCH -n 16 # Number of cores
+#SBATCH -N 1-4 # Min - Max Nodes
+#SBATCH -p all
+#SBATCH --mem=150G
+#SBATCH --time=48:00:00
+#SBATCH --output=bed_to_bigwig_2.out
+
 # Path to your directory with sorted BED files
 dir="/home/abportillo/github_repo/RNA_seq_Bcell/output/chip_exo/KLF4/kzfp_peaks"
 
